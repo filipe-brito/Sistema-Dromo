2fcea07fd02aa7f0e82afd2c420ad8e6dac675bc
Filipe Brito <filipe.bmorais@hotmail.com>
2025-03-17 22:44:41 -0300

    Configuração inicial do backend. Definição de um banco de dados H2 para teste. Conseguimos compilar o backend iicialmente

--- backend/sistema-dromo/src/main/java/com/dromo/sistema_dromo/model/User.java
+++ backend/sistema-dromo/src/main/java/com/dromo/sistema_dromo/model/User.java
@@ -1,5 +1,11 @@
 package com.dromo.sistema_dromo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 @Entity
 @Table(name = "users")
 public class User {
--- backend/sistema-dromo/src/main/java/com/dromo/sistema_dromo/repository/UserRepository.java
+++ backend/sistema-dromo/src/main/java/com/dromo/sistema_dromo/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.dromo.sistema_dromo.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dromo.sistema_dromo.model.User;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
