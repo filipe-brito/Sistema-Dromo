68e37a579dbb441cb8ce12258085478fd2f90c48
Filipe Brito de Morais <fmorais@eslsistemas.com.br>
2025-03-19 16:42:50 -0300

    Estudamos todo o conceito sobre o pacote Controller e as anotações referentes ao Spring Boot. Comentamos pontos importantes no código da classe.

--- backend/sistema-dromo/src/main/java/com/dromo/sistema_dromo/controller/UserController.java
+++ backend/sistema-dromo/src/main/java/com/dromo/sistema_dromo/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.dromo.sistema_dromo.controller;
 
-
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,16 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dromo.sistema_dromo.model.User;
 import com.dromo.sistema_dromo.service.UserService;
 
+/*
+ * Essa é uma classe que cumpre o papel de Controller, seguindo o modelo MVC. 
+ * O Controller é o componente responsável por gerenciar as requisições do usuário e coordenar a interação entre a View (interface do usuário) e o Model (dados e regras de negócio). 
+ * Ele age como um intermediário, recebendo as entradas do usuário, processando-as e decidindo qual resposta deve ser enviada de volta. 
+ */
 
 
-@RestController
-@RequestMapping("/users")
+@RestController // Anotação do Spring Boot. Define a classe como Controller que gerencia requisições REST (GET, POST, etc). Basicamente, cria uma API do sistema. Os métodos da classe retornam dados que serão automaticamente convertidos em JSON. O @RestController também cuida do tratamento de exceções.
+@RequestMapping("/users") // Anotação do Spring Boot. Por estar em nível de classe, define um prefixo que o client deve inserir na URL da requisição para acessar todos os métodos dessa classe. Também pode ser declarado em nível de método.
 public class UserController {
-	@Autowired
-	private UserService userService;
+	@Autowired // Anotação do Spring Boot. É feito automaticamente uma injeção de dependência do objeto imediatamente abaixo, transferindo a responsabilidade de criar esse objeto para o Spring Boot.
+	private UserService userService; // Classe que criamos para representar serviços e regras de negócio.
 
-	@GetMapping
-	public List<User> listUsers() {
+	@GetMapping // Anotação do Spring Boot. Está diretamente associada a classes com anotação @RestController e complementa a ação do @RequestMapping. Faz com que toda requisição GET enviada ao prefixo definido no @GetMapping execute o método imediatamente abaixo.
+	public List<User> listUsers() { // Método que chama outro método homônimo da classe de serviço.
 		return userService.listUsers();
 	}
 }
--- backend/sistema-dromo/src/main/java/com/dromo/sistema_dromo/model/User.java
+++ backend/sistema-dromo/src/main/java/com/dromo/sistema_dromo/model/User.java
@@ -14,4 +14,16 @@ public class User {
 	private Long id;
 	private String name;
 	private String email;
+	
+	public Long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	
+	
 }
