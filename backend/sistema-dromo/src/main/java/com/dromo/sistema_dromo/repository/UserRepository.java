3633d60a756a80a2c6580388e229dfc5d8bbb0ea
Filipe Brito de Morais <fmorais@eslsistemas.com.br>
2025-03-20 16:53:56 -0300

    Estudamos bastante o conceito de MVC. Adicionei um arquivo a mais na pasta 'procedimentos' com meu entendimento do padrão. Adicionamos comentários explicativos sobre o repository e model.

--- backend/sistema-dromo/src/main/java/com/dromo/sistema_dromo/repository/UserRepository.java
+++ backend/sistema-dromo/src/main/java/com/dromo/sistema_dromo/repository/UserRepository.java
@@ -1,8 +1,29 @@
 package com.dromo.sistema_dromo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.dromo.sistema_dromo.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+/*
+ * O pacote repository é o responsável por fazer a comunicação direta com o banco de dados.
+ * 
+ * Nesse pacote, usamos somente interfaces. Para cada entidade, deverá ser criada uma 
+ * interface que herda de JpaRepository.
+ * A interface JpaRepository, da biblioteca Spring Data JPA, facilita muito o processo. 
+ * Com ela, não precisamos configurar queries SQL diretamente, pois já temos métodos prontos
+ * para operações básicas de CRUD (Create, Read, Update, Delete).
+ * 
+ * Como estamos usando a biblioteca Spring Data JPA, não precisamos usar a anotação @Repository.
+ * O Spring já reconhece automaticamente interfaces que estendem JpaRepository como repositórios.
+ */
+
+/*
+ * A interface JpaRepository recebe dois parâmetros:
+ * User: entidade que foi criada e
+ * Long: tipo de valor da chave primária.
+ */
+@Repository // Anotação opcional para indicar que esta interface é um repositório.
+public interface UserRepository extends JpaRepository<User, Long> { 
+	// Métodos personalizados podem ser adicionados aqui.
 }
