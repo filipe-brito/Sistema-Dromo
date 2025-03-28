c91edabadf4a36d4e2acd5ceb7382601f36fb34a
Filipe Brito de Morais <fmorais@eslsistemas.com.br>
2025-03-28 16:26:34 -0300

    Criamos um layout básico da página de login.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppRouter from "./router"; // Aqui importamos o AppRouter
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <AppRouter /> {/* Agora renderizamos o AppRouter, que contém as rotas */}
+  </React.StrictMode>
+);
