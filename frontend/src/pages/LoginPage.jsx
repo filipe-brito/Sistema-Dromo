160011070cb7ab5c986d8233e036dfe8c55684cb
Amanda Cardozo Soares Castilho <filipe.bmorais@hotmail.com>
2025-03-29 22:20:11 -0300

    Entendemos os conceitos de JSX e como o Tailwind aplica o estilo nele. Fizemos uma boa base para uma tela de login

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,31 +1,36 @@
-import React, { useState } from "react";
+import React, { useState } from "react"; // Importa o React que vai gerenciar esse componente. Importa o hook "useStage"
 
-const LoginPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const LoginPage = () => { // No React, criamos componentes (ou páginas) usando arrow functions. 
+  const [email, setEmail] = useState(""); // Definimos um hook  useState para uma variável "email"
+  const [password, setPassword] = useState(""); // Definimos um hook useState para uma variável "password"
+  /*
+  Essa é a sintaxe para definir um hook no React. Tomemos o "email" como exemplo
+  const: define que a váriável email é imutável
+  [email, setEmail]: email é o nome da variável e setEmail é o método usado para alterar o valor de email
+  = useState(""): atribuímos o useState à essa variável e definimos uma String vazia como valor inicial 
+  */
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Email:", email, "Senha:", password);
-    // Aqui futuramente chamamos a API de autenticação
+  const handleSubmit = (e) => { // arrow funciton que será chamado por um button
+    e.preventDefault(); // evita que o recarregamento da página quando o evento que chama essa função ocorrer
+    console.log("Email:", email, "Senha:", password); // Por ora, o método só imprime no console
   };
 
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+  return ( // Padrão do React para definir um componente. Deve-se retornar um JSX
+    <div className="flex items-center justify-center h-screen w-screen bg-gray-100">
       <div className="bg-white shadow-lg rounded-lg p-8 w-96">
         <div className="flex justify-center">
-          <img src="/images/img-01.png" alt="Login" className="w-32" />
+          <img src="/images/gamer-logo.png" alt="Login" className="w-30" />
         </div>
 
-        <h2 className="text-center text-2xl font-bold text-gray-800 mt-4">Member Login</h2>
+        <h2 className="text-center text-3xl font-bold text-gray-800 mt-4">Acesse o seu Dromo!</h2>
 
-        <form className="mt-6" onSubmit={handleSubmit}>
+        <form className="mt-6" onSubmit={handleSubmit}> {/* Cria um formulário e define o evento de submissão para ele*/}
           <div className="mb-4">
             <label className="block text-gray-700">Email</label>
             <div className="relative">
               <input
                 type="email"
-                className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full p-3 border text-gray-800 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
@@ -41,7 +46,7 @@ const LoginPage = () => {
             <div className="relative">
               <input
                 type="password"
-                className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full p-3 border text-gray-800 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
