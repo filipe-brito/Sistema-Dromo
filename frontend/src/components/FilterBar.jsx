34b26f39f7abd0361c1ca30c43021f20d8dc49ed
Filipe Brito de Morais <fmorais@eslsistemas.com.br>
2025-04-04 13:36:33 -0300

    Protótipo da barra de filtros concluído. Posteriormente, quando a página estiver funcional, daremos uma atenção especial à estilização e deixar bem bunitinho

--- frontend/src/components/FilterBar.jsx
+++ frontend/src/components/FilterBar.jsx
@@ -1,21 +1,24 @@
-const FilterBar = ({ filters, onSearch }) => {
-    return (
-      <div className="bg-stone-100 p-4 rounded-lg shadow-md flex gap-4">
+const FilterBar = ({filters, onSearch}) => {
+  return(
+    <div className="flex w-full h-8">
+      <div className="flex inline-bloc w-9/10 gap-1">
         {filters.map((filter) => (
-          <input
-            key={filter.name}
-            type={filter.type || "text"}
-            placeholder={filter.placeholder}
-            className="border-2 border-neutral-900/50 px-3 py-2 rounded-md text-neutral-800"
-            onChange={(e) => onSearch(filter.name, e.target.value)}
+          <input 
+          key={filter.key}
+          type={filter.type}
+          placeholder={filter.placeholder}
+
+          className="px-1 bg-stone-200 rounded border-2 border-stone-400"
+
+          onChange={(e) => onSearch(filter.name, e.target.value)}
           />
         ))}
-        <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
-          Buscar
-        </button>
       </div>
-    );
-  };
-  
-  export default FilterBar;
-  
+      <div className="relative w-1/10 h-6">
+        <button className="absolute right-0 w-20 h-8 rounded-lg border-3 border-emerald-600 bg-emerald-300 text-white">Buscar</button>
+      </div>
+    </div>
+  );
+};
+
+export default FilterBar;
