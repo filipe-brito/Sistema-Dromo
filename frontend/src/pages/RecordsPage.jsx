0dcbbbc259c742cea293dee270b754d0032409b8
Filipe Brito de Morais <fmorais@eslsistemas.com.br>
2025-04-04 13:36:33 -0300

    Protótipo da barra de filtros concluído. Posteriormente, quando a página estiver funcional, daremos uma atenção especial à estilização e deixar bem bunitinho

--- frontend/src/pages/RecordsPage.jsx
+++ frontend/src/pages/RecordsPage.jsx
@@ -21,14 +21,16 @@ const RecordsPage = () => {
   ];
 
     return (
-    <div className="w-8/10 h-screen mx-auto p-8 bg-stone-300 shadow-lg shadow-stone-400">
-      <h2 className="text-2xl font-bold text-gray-800 mb-6">Cadastro de Pessoas</h2>
+    <div className="w-8/10 h-screen mx-auto p-4 bg-stone-300 shadow-lg shadow-stone-400">
+      <h2 className="text-3xl font-bold text-gray-800 mb-4">Cadastro de Pessoas</h2>
 
       {/*Aplicando componente de filtro*/}
+      <div className="flex p-2 rounded shadow-sm bg-stone-100">
       <FilterBar filters={filters} onSearch={handleSearch} />
+      </div>
 
       {/* Teste: Exibindo os valores digitados nos filtros */}
-      <pre className="mt-4 text-neutral-800 bg-stone-100 p-4 rounded">{JSON.stringify(searchParams, null, 2)}</pre>
+      <pre className="mt-2 text-neutral-800 bg-stone-100 p-4 rounded">{JSON.stringify(searchParams, null, 2)}</pre>
     </div>
   );
 };
