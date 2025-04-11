7238419430b99c1a0bb54c5e16904e34e9b99fc1
Filipe Brito de Morais <fmorais@eslsistemas.com.br>
2025-04-11 16:58:39 -0300

    Estilizamos bastante a página de cadastros, adicionamos ícones de pessoa física e empresa e criamos uma lógica para abas. Não ficou do jeito que eu queria por limitações em relação as bordas, mas ficou satisfatótio.

--- frontend/src/components/atoms/SearchButton.jsx
+++ frontend/src/components/atoms/SearchButton.jsx
@@ -2,8 +2,8 @@ import { SearchIcon1 } from "../icons/SearchIcon.jsx";
 
 export const SearchButton = () => {
   return (
-    <button className="group absolute right-0 p-1 rounded-md bg-violet-500 cursor-pointer">
-      <SearchIcon1 className="w-4 h-4 text-white transition-transform duration-300 group-hover:scale-110" />
+    <button className="group absolute right-0 p-1 rounded-md bg-green-700 cursor-pointer">
+      <SearchIcon1 className="w-4 h-4 text-white transition-transform duration-300 group-hover:scale-120" />
     </button>
   );
 };
--- frontend/src/components/organisms/Tabs.jsx
+++ frontend/src/components/organisms/Tabs.jsx
@@ -4,23 +4,28 @@ export const Tab = ({ tabs, defaultTab = 0 }) => {
   const [activeTab, setActiveTab] = useState(defaultTab);
 
   return (
-    <div className="w-full">
-      <div className="flex">
+    <div className="relative w-full">
+      {/* 🔹 Aba sobreposta */}
+      <div className="flex gap-1 relative mx-2">
         {tabs.map((tab, index) => (
           <button
             key={index}
             onClick={() => setActiveTab(index)}
-            className={`p-1 ${
+            className={`px-1 rounded-t-md font-semibold border cursor-pointer flex gap-2 items-center ${
               index === activeTab
-                ? "text-blue-400 border-b-2 border-bluw-400"
-                : "text-green-400"
+                ? "text-neutral-100 bg-green-700 border-transparent"
+                : "text-neutral-100/50 bg-transparent border-green-400/60 rounded transition-colors duration-300 hover:text-neutral-100"
             }`}
           >
+            {tab.icon}
             {tab.label}
           </button>
         ))}
       </div>
-      <div className="p-2">{tabs[activeTab].content}</div>
+      {/* 🔹 Conteúdo com borda total */}
+      <div className="p-1 border-2 border-green-700 rounded-md">
+        {tabs[activeTab].content}
+      </div>
     </div>
   );
 };
