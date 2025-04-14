eebda902e8b17ebf64e595bd84604c8e1908b465
Filipe Brito de Morais <fmorais@eslsistemas.com.br>
2025-04-14 16:52:24 -0300

    Criamos uma nova tabela no banco de dados para Empresas e já implementamos a busca no frontend. Melhoramos a estrutura de pastas do frontend seguindo o padrão de Atomic Design

--- backend/sistema-dromo/src/main/java/com/dromo/sistema_dromo/controller/RecordsController.java
+++ backend/sistema-dromo/src/main/java/com/dromo/sistema_dromo/controller/RecordsController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dromo.sistema_dromo.dto.CompanyDTO;
 import com.dromo.sistema_dromo.dto.IndividualDTO;
+import com.dromo.sistema_dromo.service.CompanyService;
 import com.dromo.sistema_dromo.service.IndividualService;
 
 @RestController
@@ -15,9 +17,16 @@ import com.dromo.sistema_dromo.service.IndividualService;
 public class RecordsController {
 	@Autowired
 	private IndividualService individualService;
+	@Autowired
+	private CompanyService companyService;
 	
 	@GetMapping("/individuals")
 	public List<IndividualDTO> listIndividuals(){
 		return individualService.listIndividuals();
+	}
+	
+	@GetMapping("/companies")
+	public List<CompanyDTO> listCompanies(){
+		return companyService.listCompanies();
 	}
 }
---
+++ backend/sistema-dromo/src/main/java/com/dromo/sistema_dromo/service/CompanyService.java
@@ -0,0 +1,26 @@
+package com.dromo.sistema_dromo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dromo.sistema_dromo.dto.CompanyDTO;
+import com.dromo.sistema_dromo.repository.CompanyRepository;
+
+@Service
+public class CompanyService {
+	@Autowired
+	private CompanyRepository companyRepository;
+	
+	public List<CompanyDTO> listCompanies(){
+		return companyRepository.findAll()
+			.stream()
+			.map(company -> new CompanyDTO(
+					company.getCompanyName(),
+					company.getCnpj()
+				))
+			.toList();
+	}
+
+}
