dd3a30bf191a081d6ed290182e69209e1ecde7b7
Filipe Brito de Morais <fmorais@eslsistemas.com.br>
2025-04-17 16:56:02 -0300

    Começamos a aplicar a lógica dos filtros. Para isso, ajustamos alguns pontos na página de cadastros e alguns componentes.

--- frontend/src/components/molecules/FilterBar.jsx
+++ frontend/src/components/molecules/FilterBar.jsx
@@ -1,6 +1,18 @@
 import { SearchButton } from "../atoms/SearchButton";
 
-const FilterBar = ({ filters }) => {
+const FilterBar = ({ filters, values, setValues, onSearch }) => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSearch = () => {
+    onSearch(values); // envia os filtros para o componente pai
+  };
+
   return (
     <div className="flex w-full">
       <div className="flex w-9/10 gap-1">
@@ -8,13 +20,16 @@ const FilterBar = ({ filters }) => {
           <input
             key={filter.name}
             type={filter.type}
+            name={filter.name}
             placeholder={filter.placeholder}
+            value={values[filter.name] || ""}
+            onChange={handleChange}
             className="h-6 px-1 bg-stone-200 rounded border-2 border-stone-400 text-xs"
           />
         ))}
       </div>
       <div className="relative w-1/10 h-6">
-        <SearchButton />
+        <SearchButton onClick={handleSearch} />
       </div>
     </div>
   );
--- frontend/src/components/templates/SearchSection.jsx
+++ frontend/src/components/templates/SearchSection.jsx
@@ -1,13 +1,26 @@
 import FilterBar from "../molecules/FilterBar";
 import ResultBar from "../molecules/ResultBar";
 import { LoadingIcon } from "../icons/LoadingIcon";
-import React from "react";
+import React, { useState } from "react";
 
-export const SearchSection = ({ filters, columns, data, loading }) => {
+export const SearchSection = ({
+  filters,
+  columns,
+  data,
+  loading,
+  onSearch,
+}) => {
+  const [filterValues, setFilterValues] = useState({});
+
   return (
     <React.Fragment>
       <div className="flex p-2 rounded shadow-sm bg-stone-100">
-        <FilterBar filters={filters} />
+        <FilterBar
+          filters={filters}
+          values={filterValues}
+          setValues={setFilterValues}
+          onSearch={() => onSearch(filterValues)}
+        />
       </div>
       <div className="flex p-2 mt-1 rounded border-stone-700 shadow-sm bg-stone-100">
         {loading ? (
