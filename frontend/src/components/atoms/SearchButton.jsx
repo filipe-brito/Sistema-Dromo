f359b3f6e0d7a0e8e51def1db7fd0cb17d31bf4f
Amanda Castilho <filipe.bmorais@hotmail.com>
2025-04-20 22:15:42 -0300

    Colocamos uma lógica de busca no botão dos filtros. Precisamos colocar a lógica de busca no backend e arrumar a lógica do front, pois, no momento, ao clicar no botão ele está atualizando o componente inteiro.

--- frontend/src/components/atoms/SearchButton.jsx
+++ frontend/src/components/atoms/SearchButton.jsx
@@ -1,8 +1,8 @@
 import { SearchIcon1 } from "../icons/SearchIcon.jsx";
 
-export const SearchButton = () => {
+export const SearchButton = ({ onClick }) => {
   return (
-    <button className="group absolute right-0 p-1 rounded-md bg-green-700 cursor-pointer">
+    <button onClick={onClick} className="group absolute right-0 p-1 rounded-md bg-green-700 cursor-pointer">
       <SearchIcon1 className="w-4 h-4 text-white transition-transform duration-300 group-hover:scale-120" />
     </button>
   );
