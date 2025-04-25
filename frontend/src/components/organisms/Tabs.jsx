a135981b0ae56b67cbc249eea20cc6f4eb05a8f9
Filipe Brito de Morais <fmorais@eslsistemas.com.br>
2025-04-25 16:57:30 -0300

    Comentamos o componente de requisição ao backend e Tabs. Muita coisa ficou clara nesse estudo. E magicamente, o bug da FilterBar desapareceu e agora está funcionando legal

--- frontend/src/components/organisms/Tabs.jsx
+++ frontend/src/components/organisms/Tabs.jsx
@@ -1,30 +1,41 @@
 import { useState } from "react";
 
 export const Tab = ({ tabs, defaultTab = 0 }) => {
-  const [activeTab, setActiveTab] = useState(defaultTab);
+  // Cria o componente e faz uma exportação nomeada. Recebe 2 props. defaulTab terá 0 como valor padrão, caso não seja enviado nenhum
+  const [activeTab, setActiveTab] = useState(defaultTab); // State para atualizar a guia ativa pelo index
 
   return (
+    // Componente que será renderizado
     <div className="relative w-full">
       {/* 🔹 Aba sobreposta */}
       <div className="flex gap-1 relative mx-2">
-        {tabs.map((tab, index) => (
-          <button
-            key={index}
-            onClick={() => setActiveTab(index)}
-            className={`px-1 rounded-t-md font-semibold border cursor-pointer flex gap-2 items-center ${
-              index === activeTab
-                ? "text-neutral-100 bg-green-700 border-transparent"
-                : "text-neutral-100/50 bg-transparent border-green-400/60 rounded transition-colors duration-300 hover:text-neutral-100"
-            }`}
-          >
-            {tab.icon}
-            {tab.label}
-          </button>
-        ))}
+        {tabs.map(
+          (
+            tab,
+            index // map é um método js para percorrer listas. tab é o item analisado e index é a posição do item no array
+          ) => (
+            //Dentro da arrow function, está a função que deverá ser executada a cada iteração
+            <button // Criamos um botão para selecionar a aba
+              key={index} // key é uma propriedade nativa do React para rastrear elementos renderizados dinamicamente como no .map
+              onClick={() => setActiveTab(index)} // onClick é a ação a ser executada ao clicar no botão. No caso, altera o estado de activeTab para alternar a aba ativa
+              className={`px-1 rounded-t-md font-semibold border cursor-pointer flex gap-2 items-center ${
+                index === activeTab // Enquando varre o array, verificamos se o index iterado é igual ao valor da aba ativa
+                  ? "text-neutral-100 bg-green-700 border-transparent" // Caso 'true', aplicamos um estilo no botão
+                  : "text-neutral-100/50 bg-transparent border-green-400/60 rounded transition-colors duration-300 hover:text-neutral-100" // Caso 'false', aplicamos outro estilo nesse botão
+              }`}
+            >
+              {tab.icon}{" "}
+              {/* Exibimos no botão o icone recebido em um dos objects da prop tabs */}
+              {tab.label}{" "}
+              {/* Exibimos no botão o rótulo recebido em um dos objects da prop tabs */}
+            </button>
+          )
+        )}
       </div>
       {/* 🔹 Conteúdo com borda total */}
       <div className="p-1 border-2 border-green-700 rounded-md">
-        {tabs[activeTab].content}
+        {tabs[activeTab].content}{" "}
+        {/* Exibimos o object content da prop tabs. Como tabs é um array, selecionamos o elemento na posíção do estado activeTab */}
       </div>
     </div>
   );
--- frontend/src/services/PeopleService.jsx
+++ frontend/src/services/PeopleService.jsx
@@ -1,18 +1,25 @@
+// Função assíncrona exportada que faz uma chamada à API
+// Recebe como argumento um objeto 'filters'. Se nenhum for passado, usa um objeto vazio por padrão
 export const fetchIndividuals = async (filters = {}) => {
-  const query = new URLSearchParams(filters).toString();
-  const response = await fetch(`http://localhost:8080/records/individuals${query ? `${query}` : ""}`);
+  const query = new URLSearchParams(filters).toString(); // Converte 'filters' em uma string de parâmetros de URL (query string)
+  const response = await fetch(
+    `http://localhost:8080/records/individuals${query ? `?${query}` : ""}`
+  ); // faz a requisição HTTP para a rota de PF
   console.log("query do PF: " + query);
 
   if (!response.ok) {
-    throw new Error("Erro ao buscar dados dos indivíduos");
+    // Verifica se a requisição deu erro
+    throw new Error("Erro ao buscar dados dos indivíduos"); // lança uma exception
   }
 
-  return response.json();
+  return response.json(); // Retorne a resposta da API
 };
 
 export const fetchCompanies = async (filters = {}) => {
   const query = new URLSearchParams(filters).toString();
-  const response = await fetch(`http://localhost:8080/records/companies${query ? `${query}` : ""}`);
+  const response = await fetch(
+    `http://localhost:8080/records/companies${query ? `?${query}` : ""}`
+  );
   console.log("query do PJ: " + query);
 
   if (!response.ok) {
