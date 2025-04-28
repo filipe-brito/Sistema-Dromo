927e78c47af9add07c29b0a792a6d756e4a9f5d6
Filipe Brito <filipe.bmorais@hotmail.com>
2025-04-27 22:59:26 -0300

    Estudamos bastante o SearchSection e agora estamos desvendo o FilterBar, que aplica conceitos de estado, operador spread e talz. É bem complexa, mas vamos entender. Todos os componentes foram comentados para melhor assimilação.

--- frontend/src/components/molecules/FilterBar.jsx
+++ frontend/src/components/molecules/FilterBar.jsx
@@ -1,7 +1,15 @@
 import { SearchButton } from "../atoms/SearchButton";
 
-const FilterBar = ({ filters, values, setValues, onSearch }) => {
+const FilterBar = ({ filters, values, setValues, onSearch }) => { // Criamos o componente FilterBar (barra de filtros para pasquisa)
+  // Variável que recebe uma função que vai criar um objeto do tipo 'object'. 
+  // Como será chamado no onChande de um input, o argumento será o 'evento' do usuário digitando nos campos
   const handleChange = (e) => {
+    // A variável abaixo é uma desestruturação de um objeto 'object'
+    // 'e' é o evento (usuário digita) e target é o próprio input que foi digitado
+    // O input digitado terá duas propriedade HTML: name e value
+    // Essa desestruturação criará duas variáveis honônimas às propriedades HTML e cada uma
+    // recebe o valor armazenado nessas propriedades. No caso,
+    // 'name' receberá o nome do campo e 'value' receberá o valor do campo (o que o usuário digitou)
     const { name, value } = e.target;
     setValues((prev) => ({
       ...prev,
--- frontend/src/components/templates/SearchSection.jsx
+++ frontend/src/components/templates/SearchSection.jsx
@@ -3,33 +3,41 @@ import ResultBar from "../molecules/ResultBar";
 import { LoadingIcon } from "../icons/LoadingIcon";
 import React, { useState } from "react";
 
-export const SearchSection = ({
-  filters,
-  columns,
-  data,
-  loading,
-  onSearch,
-}) => {
-  const [filterValues, setFilterValues] = useState({});
+export const SearchSection = ({ // Criamos o componente SearchSection e usamos exportação nomeada
+  // Seguem props
+  filters, // Array de objects com as informações dos campos da barra de filtro
+  columns, // Array de objects com as informações das colunas da barra de resultados
+  data, // Dados que serão apresentados na barra de resultados
+  loading, // Estado para exibir o ícone de loading enquanto os dados carregam
+  onSearch, // Ação do botão de pesquisar da barra de filtros
+}) => { // Arrow function que esse componente irá executar
+  const [filterValues, setFilterValues] = useState({}); // Estado que controla os valores dos campos de filtros que serão digitados
 
   return (
-    <React.Fragment>
+    <React.Fragment> {/* Em React, todo componente precisa retornar um único elemento pai. 
+      React.Fragment é uma forma de agrupar múltiplos elementos sem adicionar uma div extra no DOM. */}
       <div className="flex p-2 rounded shadow-sm bg-stone-100">
         <FilterBar
-          filters={filters}
-          values={filterValues}
-          setValues={setFilterValues}
+          filters={filters} // Passamos a prop recebida com pai para FilterBar
+          values={filterValues} // passamos a variável do estado para o FilterBar exibir
+          setValues={setFilterValues} // passamos a função do estado para FilterBar alterar
+
+          // OnSearch recebido do pai deverá ser uma função de consulta ao backend e deverá aceitar argumentos,
+          // pois os argumentos serão parâmetros de busca no banco de dados
+          // O componente atual recebe uma função do pai e passará para seu filho, então usamos um função anônima e passamos o argumento necessário
           onSearch={() => onSearch(filterValues)}
         />
       </div>
       <div className="flex p-2 mt-1 rounded border-stone-700 shadow-sm bg-stone-100">
-        {loading ? (
+        {loading ? ( // Lógica do operador ternário verifica se o valor de loading é 'true' ou 'false'
+          // Caso 'true', exibe o ícone de loading
           <div className="flex w-full justify-center">
             <LoadingIcon />
             <p>Carregando...</p>
           </div>
         ) : (
-          <ResultBar columns={columns} data={data} />
+          // caso 'false', carrega a barra de resultados
+          <ResultBar columns={columns} data={data} /> // Passamos as colunas e os dados que serão exibidos nessas colunas
         )}
       </div>
     </React.Fragment>
