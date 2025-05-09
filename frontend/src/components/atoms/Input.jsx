03291616bfdc16279cf6c8c8b9cc627360a0752a
Filipe Brito de Morais <fmorais@eslsistemas.com.br>
2025-05-09 15:59:05 -0300

    Iniciamos a página de edição de cadastro. Por enquanto, estou só testando e tentando encontrar a melhor forma de controlar inputs.

---
+++ frontend/src/components/atoms/Input.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { IMaskInput } from "react-imask";
+
+// Input padrão do sistema
+export const DefaultInput = ({
+  key,
+  name,
+  label,
+  placeholder,
+  inputStyle,
+  type,
+  value,
+  onChange,
+}) => {
+  return (
+    <div className="flex flex-col">
+      <label className="text-sm">{label}</label>
+      <input
+        name={name}
+        type={type ? type : "text"}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        className={`h-6 px-1 bg-stone-200 rounded border-2 border-stone-400 text-xs ${
+          inputStyle ? inputStyle : "w-40"
+        }`}
+      />
+    </div>
+  );
+};
+
+// Input com máscara
+export const MaskedInput = ({
+  label,
+  mask,
+  name,
+  placeholder,
+  value,
+  onChange,
+  inputStyle,
+}) => {
+  return (
+    <div className="flex flex-col">
+      <label className="text-sm">{label}</label>
+      <IMaskInput
+        // Cada object interado tem seus atributos. Vamos atribuir esses atributos adequadamente ao input
+        mask={mask} // aplica a máscara no campo
+        name={name} // Atribui o valor da chave 'name' ao nome do input
+        placeholder={placeholder} // O texto exibido no campo será o valor da chave 'placeholder'
+        value={value} // controla o que está digitado no campo. Os colchetes diz ao React para pegar o valor da chave daquele object
+        onChange={onChange} // Executa handleChange a cada evento. React envia o próprio evento como argumento
+        className={`h-6 px-1 bg-stone-200 rounded border-2 border-stone-400 text-xs ${
+          inputStyle ? inputStyle : "w-40"
+        }`}
+      />
+    </div>
+  );
+};
--- frontend/src/pages/TestPage.jsx
+++ frontend/src/pages/TestPage.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
 import { Tab } from "../components/templates/Tabs";
 import { PersonIcon } from "../components/atoms/icons/PersonIcon";
+import { DefaultInput, MaskedInput } from "../components/atoms/Input";
 
 const TestPage = () => {
+  // Vamos criar um hook form
+  const {
+    register, // função que registra o campo no formulário
+    // handleSSubmit função que lida com o envio do formulário.
+    // Ela vai validar se os campos foram preenchidos devidamente antes de chamar uma requisição ao BD
+    handleSubmit,
+    control, // função que registra os campos que não são nativos (inputs)
+    // formState é objeto com os erros de validação. A chave seria o nome do campo e o valor seria a mensagem a ser apresentada
+    // caso o usuário tente submeter o formulário com algum campo obrigatório vazio
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    // Função principal que define a ação ao submeter o formulário
+    console.log("Dados enviados:", data);
+  };
+
   return (
     <React.Fragment>
       <div className="w-8/10 min-h-[92dvh] mx-auto px-4 py-2 bg-stone-800 border-x-2 border-stone-700">
@@ -14,8 +33,73 @@ const TestPage = () => {
           tabs={[
             {
               icon: <PersonIcon className="w-4 h-4" />,
-              label: "Dados principais",
-              content: <section></section>,
+              label: "Dados pessoais",
+              content: (
+                <React.Fragment>
+                  <section className="flex flex-wrap w-full p-2 rounded border-stone-700 shadow-sm bg-stone-100 gap-1">
+                    <form
+                      id="save"
+                      onSubmit={handleSubmit(onSubmit)}
+                      className="flex gap-2"
+                    >
+                      {/* Input Nativo */}
+                      <div>
+                        <Controller
+                          defaultValue="" // <== Isso aqui evita o warning
+                          name="name"
+                          control={control}
+                          rules={{ required: "Nome é obrigatório" }}
+                          render={({ field }) => (
+                            <DefaultInput {...field} label="Nome" />
+                          )}
+                        />
+                        {errors.nome && (
+                          <span className="text-red-500">
+                            {errors.nome.message}
+                          </span>
+                        )}
+                      </div>
+
+                      {/* Input com Máscara (CPF) */}
+
+                      <Controller // Componente do HookForm necessário para inputs não-nativos
+                        defaultValue="" // <== Isso aqui evita o warning
+                        name="cpf" // Nome do campo
+                        control={control} // Estado para controlar o input fornecido pelo HookFrom
+                        rules={{
+                          required: "O CPF é obrigatório",
+                          validate: (value) =>
+                            /^\d{3}\.\d{3}\.\d{3}-\d{2}$/.test(value) ||
+                            "CPF inválido",
+                        }} // Demais regras. No caso, definimos este um input obrigatório
+                        render={(
+                          { field } // Função principal para renderizar o campo. A prop field é fornecida pelo Controller
+                        ) => (
+                          <MaskedInput
+                            // Guarda as propriedades de field antes de adicionar as novas abaixo
+                            {...field}
+                            label="CPF"
+                            mask="000.000.000-00"
+                            placeholder="000.000.000-00"
+                          />
+                        )}
+                      />
+                      {errors.cpf && (
+                        <span className="text-red-500 text-xs">
+                          {errors.cpf.message}
+                        </span>
+                      )}
+                    </form>
+                  </section>
+                  <button
+                    form="save"
+                    type="submit"
+                    className="bg-blue-500 text-white px-3 py-1 rounded fixed right-30 bottom-5"
+                  >
+                    Salvar
+                  </button>
+                </React.Fragment>
+              ),
             },
           ]}
         />
