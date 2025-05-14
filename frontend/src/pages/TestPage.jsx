304bea1e34bfe65cf6d64dfa7cc3511149867f05
Filipe Brito de Morais <fmorais@eslsistemas.com.br>
2025-05-14 16:51:32 -0300

    O protótipo do construtor de formulários está pronto. Vamos sofisticá-lo para aceitar 'seções de cadastro'. Também já testamos o componente na página de edição de cadastros. Está ficando legal

--- frontend/src/pages/TestPage.jsx
+++ frontend/src/pages/TestPage.jsx
@@ -2,7 +2,11 @@ import React, { useState } from "react";
 import { FormBuilder } from "../components/organisms/FormBuilder";
 import { Tab } from "../components/templates/Tabs";
 import { PersonIcon } from "../components/atoms/icons/PersonIcon";
-import { MaskedInput } from "../components/atoms/Input";
+import {
+  MaskedInput,
+  DefaultInput,
+  SelectInput,
+} from "../components/atoms/Input";
 import { useForm, Controller } from "react-hook-form";
 import { validators } from "../utils/validators";
 
@@ -26,21 +30,40 @@ const TestPage = () => {
   const inputs = [
     {
       name: "cpf",
+      type: "masked",
       label: "CPF",
       mask: "000.000.000-00",
       placeholder: "Ex: 999.999.999-99",
+      required: "CPF é obrigatório",
     },
     {
       name: "cnpj",
+      type: "masked",
       label: "CNPJ",
       mask: "00.000.000/0000-00",
       placeholder: "Ex: 99.999.999/9999-99",
+      required: "CNPJ é obrigatório",
     },
     {
       name: "telefone",
+      type: "masked",
       label: "Telefone",
-      mask: "(00)0000-0000",
+      mask: "(00) 0000-0000",
       placeholder: "Ex: (99) 9999-9999",
+    },
+    {
+      name: "gender",
+      type: "select",
+      label: "Sexo",
+      options: [{ value: "Masculino" }, { value: "Feminino" }],
+      required: "Sexo é obrigatório",
+      inputStyle: "w-22",
+    },
+    {
+      name: "email",
+      type: "default",
+      type2: "email",
+      label: "Email",
     },
   ];
 
@@ -57,48 +80,118 @@ const TestPage = () => {
               icon: <PersonIcon className="w-4 h-4" />,
               label: "Dados pessoais",
               content: (
-                <React.Fragment>
-                  <div className="flex-col" key={inputs[1].name}>
-                    {errors[inputs[1].name] && (
-                      <span className="text-red-500 text-xs">
-                        {errors[inputs[1].name].message}
-                      </span>
-                    )}
-                    <Controller // Componente do HookForm necessário para inputs não-nativos
-                      defaultValue="" // <== Isso aqui evita o warning
-                      name={inputs[1].name} // Nome do campo
-                      control={control} // Estado para controlar o input fornecido pelo HookForm
-                      rules={{
-                        required: validators[inputs[1].name].requiredMessage,
-                        validate: validators[inputs[1].name].validator,
-                      }} // Demais regras. No caso, definimos este um input obrigatório
-                      render={(
-                        { field } // Função principal para renderizar o campo. A prop field é fornecida pelo Controller
-                      ) => (
-                        <MaskedInput
-                          // Guarda as propriedades de field antes de adicionar as novas abaixo
-                          {...field}
-                          label={inputs[1].label}
-                          mask={inputs[1].mask}
-                          placeholder={inputs[1].mask}
-                        />
-                      )}
-                    />
-                  </div>
-                  <FormBuilder inputs={inputs} onSubmit={onSubmit} />
-                  <button
-                    form="save"
-                    type="submit"
-                    className="bg-blue-500 text-white px-3 py-1 rounded fixed right-30 bottom-5"
+                <section className="flex p-2 rounded shadow-sm bg-stone-100">
+                  <form
+                    id="save"
+                    onSubmit={handleSubmit(onSubmit)}
+                    className="flex gap-2"
                   >
-                    Salvar
-                  </button>
-                </React.Fragment>
+                    {inputs.map((input) => {
+                      switch (input.type) {
+                        case "default":
+                          return (
+                            <div className="flex-col" key={input.name}>
+                              {errors[input.name] && (
+                                <span className="text-red-500 text-xs">
+                                  {errors[input.name].message}
+                                </span>
+                              )}
+                              <Controller
+                                defaultValue="" // <== Isso aqui evita o warning no navegador
+                                name={input.name}
+                                control={control}
+                                rules={{ required: input?.required }}
+                                render={({ field }) => (
+                                  <DefaultInput
+                                    {...field}
+                                    label={input.label}
+                                    placeholder={input.placeholder}
+                                  />
+                                )}
+                              />
+                            </div>
+                          );
+                        case "masked":
+                          return (
+                            <div className="flex-col" key={input.name}>
+                              <span>
+                                {errors[input.name] && (
+                                  <span className="text-red-500 text-xs">
+                                    {errors[input.name].message}
+                                  </span>
+                                )}
+                              </span>
+                              <Controller // Componente do HookForm necessário para inputs não-nativos
+                                defaultValue="" // <== Isso aqui evita o warning
+                                name={input.name} // Nome do campo
+                                control={control} // Estado para controlar o input fornecido pelo HookForm
+                                rules={{
+                                  required: input?.required,
+                                  validate: validators[input.name]?.validator,
+                                }} // Demais regras. No caso, definimos este um input obrigatório
+                                render={(
+                                  { field } // Função principal para renderizar o campo. A prop field é fornecida pelo Controller
+                                ) => (
+                                  <MaskedInput
+                                    // Guarda as propriedades de field antes de adicionar as novas abaixo
+                                    {...field}
+                                    label={input.label}
+                                    mask={input.mask}
+                                    placeholder={input.placeholder}
+                                  />
+                                )}
+                              />
+                            </div>
+                          );
+
+                        case "select":
+                          return (
+                            <div className="flex-col" key={input.name}>
+                              <span>
+                                {errors[input.name] && (
+                                  <span className="text-red-500 text-xs">
+                                    {errors[input.name].message}
+                                  </span>
+                                )}
+                              </span>
+                              <Controller // Componente do HookForm necessário para inputs não-nativos
+                                defaultValue="" // <== Isso aqui evita o warning
+                                name={input.name} // Nome do campo
+                                control={control} // Estado para controlar o input fornecido pelo HookForm
+                                rules={{
+                                  required: input?.required,
+                                }} // Demais regras. No caso, definimos este um input obrigatório
+                                render={(
+                                  { field } // Função principal para renderizar o campo. A prop field é fornecida pelo Controller
+                                ) => (
+                                  <SelectInput
+                                    {...field}
+                                    name={input.name}
+                                    label={input.label}
+                                    options={input.options}
+                                    inputStyle={input.inputStyle}
+                                  />
+                                )}
+                              />
+                            </div>
+                          );
+                      }
+                    })}
+                  </form>
+                </section>
               ),
             },
           ]}
         />
       </div>
+
+      <button
+        form="save"
+        type="submit"
+        className="bg-blue-500 text-white px-3 py-1 rounded fixed right-30 bottom-5"
+      >
+        Salvar
+      </button>
     </React.Fragment>
   );
 };
