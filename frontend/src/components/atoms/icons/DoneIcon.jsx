b74359b45ad141a316ba59512d02d9310e975568
Filipe Brito de Morais <fmorais@eslsistemas.com.br>
2025-05-23 16:58:43 -0300

    Estilizamos bem o modal de confirmação e adicionamos ícones de carregamento e de retorno da requisição. Ficou bem legal. A página para criar um novo cadastro de PF está finalizada. Os próximos passos são aplicar a mesma lógica de criação para PJ e criar uma página de edição de cadastros

---
+++ frontend/src/components/atoms/icons/DoneIcon.jsx
@@ -0,0 +1,18 @@
+export function DoneIcon(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M12 21a9 9 0 0 0 7.51-13.961l-7.155 7.95a2 2 0 0 1-2.687.262L6.4 12.8a1 1 0 0 1 1.2-1.6l3.268 2.451l7.346-8.161A9 9 0 1 0 12 21"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+}
--- frontend/src/pages/TestPage.jsx
+++ frontend/src/pages/TestPage.jsx
@@ -1,10 +1,23 @@
 import React, { useState } from "react";
 import { ConfirmModal } from "../components/molecules/ConfirmModal";
+import { DoneIcon } from "../components/atoms/icons/DoneIcon";
+import { LoadingIcon } from "../components/atoms/icons/LoadingIcon";
 
 const TestPage = () => {
   return (
     <React.Fragment>
-      <ConfirmModal />
+      <div className="fixed inset-0 flex items-center justify-center bg-black/70 z-50">
+        <div className="bg-white flex flex-col items-center p-6 rounded gap-y-4">
+          <DoneIcon fill="green" className={"w-15 h-15 text-green-600"} />
+          <p>Cadastro realizado!</p>
+          <button
+            onClick={() => setConfirmOpen(false)}
+            className="px-3 py-1 bg-blue-600 text-white rounded cursor-pointer hover:bg-blue-500"
+          >
+            Fechar
+          </button>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
