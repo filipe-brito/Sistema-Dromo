dae1c35a7b9a9e24b8635cb99a5e51f7b6bd77e4
Filipe Brito de Morais <fmorais@eslsistemas.com.br>
2025-06-17 16:47:30 -0300

    Implementamos a autenticação na página de login. Devemos modificar todos as demais requisições para carregar o token

--- backend/sistema-dromo/src/main/java/com/dromo/sistema_dromo/security/SecurityConfig.java
+++ backend/sistema-dromo/src/main/java/com/dromo/sistema_dromo/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.dromo.sistema_dromo.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,6 +16,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.dromo.sistema_dromo.security.user.UserDetailsServiceImpl; // Seu novo UserDetailsServiceImpl
 
@@ -49,6 +54,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable())
+				.cors(cors -> cors.configurationSource(corsConfigurationSource()))
 				.authorizeHttpRequests(
 						auth -> auth.requestMatchers("/auth/login").permitAll()
 						.anyRequest().authenticated())
@@ -146,6 +152,19 @@ public class SecurityConfig {
 		 * cliente, retorna true.
 		 */
 	}
+	
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 	@Bean // Define o codificador de senhas (BCrypt para hashing)
 	public PasswordEncoder passwordEncoder() {
