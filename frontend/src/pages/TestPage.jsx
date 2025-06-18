c4be3432c4db61fb0d819f82c74aeefa7d797822
Filipe Brito de Morais <fmorais@eslsistemas.com.br>
2025-06-18 16:42:22 -0300

    Resolvemos um bug ao carregar a página de edição das companias. Tratamos a falha de login com credenciais incorretas e implementamos um botão com dropdown no header para o usuário fazer logout, limpar o token do localStorage e redirecionar para a página de login. O programa está funcional, podemos subir para produção.

--- frontend/src/pages/TestPage.jsx
+++ frontend/src/pages/TestPage.jsx
@@ -1,28 +1,42 @@
-import React, { useState } from "react";
-import { EditButton } from "../components/atoms/EditButton";
-import { NewFooter } from "../components/organisms/Footer";
-import { SaveButton } from "../components/atoms/SaveButton";
+import { UserIcon } from "../components/atoms/icons/UserIcon";
+import { useContext, useState } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthContext";
+import { LogoutIcon } from "../components/atoms/icons/LogoutIcon";
 
 const TestPage = () => {
-  const [isConfirmOpen, setConfirmOpen] = useState(false);
-  const [status, setStatus] = useState("idle"); // idle | loading | success | error
+  const [userDropDown, setUserDropDown] = useState(false);
+  const { logout } = useContext(AuthContext);
 
-  const footerButtons = () => (
-    <React.Fragment>
-      <SaveButton className="absolute right-6" />
-      <EditButton />
-    </React.Fragment>
-  );
+  const handleLogout = () => {
+    logout();
+    Navigate("/login");
+  };
 
   return (
-    <React.Fragment>
-      <div className="w-8/10 min-h-[92dvh] mx-auto px-4 py-2 rounded-b bg-stone-800 border-x-2 border-b-2 border-stone-700">
-        <div className="w-full bg-green-900 h-500">
-          <h1>Teste maroto</h1>
-        </div>
+    <div className="flex items-center justify-center h-screen w-screen bg-stone-900">
+      <div className="relative">
+        <button
+          onClick={() => setUserDropDown(!userDropDown)}
+          className="flex items-center group rounded-2xl bg-black/40 w-16 hover:outline-2 outline-white/40 cursor-pointer active:bg-black/20"
+        >
+          <UserIcon className="text-green-700 h-10 w-10" />
+          <span className="text-white/20 group-hover:text-white/40">
+            &#9660;
+          </span>
+        </button>
+        {userDropDown && (
+          <ul className="absolute mt-3 w-30 bg-stone-300 text-neutral-800 shadow-lg rounded-md">
+            <li className="flex items-center group hover:bg-stone-200 rounded-md">
+              <button onclick={handleLogout} className="flex w-full px-2 py-2">
+                <LogoutIcon className="text-neutral-600/70 group-hover:text-neutral-600/100" />
+                Sair
+              </button>
+            </li>
+          </ul>
+        )}
       </div>
-      <NewFooter />
-    </React.Fragment>
+    </div>
   );
 };
 
