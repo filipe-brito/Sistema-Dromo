b1d872646caa1ff58ce320932f1a86f03d12da88
Filipe Brito <filipe@reinodosatrozes.filipixel>
2025-06-22 09:53:35 -0300

    Está no ar! Subimos o projeto para o servidor e já está publicado na web! Tivemos alguns problemas com CORS, pois agora essa configuração deve ser feita no método corsConfigurationSource da classe SecurityConfig, e o domínio que os clientes vão acessar não constava lá. Após fazer a alteração fuincionou perfeitamente. Agora, é revisar todo o conteúdo desse projeto full stack.

--- backend/sistema-dromo/src/main/java/com/dromo/sistema_dromo/security/SecurityConfig.java
+++ backend/sistema-dromo/src/main/java/com/dromo/sistema_dromo/security/SecurityConfig.java
@@ -156,7 +156,7 @@ public class SecurityConfig {
 	@Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://dromo.filipixel.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
