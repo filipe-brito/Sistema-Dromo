8e54946c165a206d23ac003219392684859d3a25
Filipe Brito de Morais <fmorais@eslsistemas.com.br>
2025-07-14 14:21:41 -0300

    Esqueci de commitar o novo arquivo de inputs.

---
+++ frontend/src/features/records/people/pages/PeopleInputs.js
@@ -0,0 +1,87 @@
+import { FetchCity } from "../../../../services/UtilsService";
+
+export const IndividualInputs = [
+  {
+    name: "name",
+    type: "default",
+    label: "Nome",
+    required: "Nome é obrigatório",
+  },
+  {
+    name: "cpf",
+    type: "masked",
+    label: "CPF",
+    required: "CPF é obrigatório",
+    mask: "000.000.000-00",
+    placeholder: "Ex: 999.999.999-99",
+  },
+  {
+    name: "gender",
+    type: "select",
+    label: "Sexo",
+    required: "Sexo é obrigatório",
+    options: [
+      { optionLabel: "masculino", value: "M" },
+      { optionLabel: "feminina", value: "F" },
+    ],
+    inputStyle: "w-25",
+  },
+  {
+    name: "maritalStatus",
+    type: "select",
+    label: "Estado civil",
+    options: [
+      { optionLabel: "Solteiro", value: "solteiro" },
+      { optionLabel: "Casado", value: "casado" },
+    ],
+    inputStyle: "w-25",
+  },
+  {
+    name: "phone",
+    type: "masked",
+    label: "Telefone",
+    mask: "(00) 0000-0000",
+    placeholder: "Ex: (99) 9999-9999",
+  },
+  {
+    name: "cellphone",
+    type: "masked",
+    label: "Celular",
+    mask: "(00) 0 0000-0000",
+    placeholder: "Ex: (99) 9 9999-9999",
+  },
+  {
+    name: "dob",
+    type: "default",
+    type2: "date",
+    label: "Data de nascimento",
+    required: "Data de nascimento é obrigatório",
+    inputStyle: "w-30",
+  },
+  {
+    name: "rg",
+    type: "default",
+    label: "RG",
+    inputStyle: "w-30",
+  },
+  {
+    name: "rntrc",
+    type: "default",
+    label: "RNTRC",
+    inputStyle: "w-30",
+  },
+  {
+    name: "email",
+    type: "default",
+    type2: "email",
+    label: "Email",
+    inputStyle: "w-50",
+  },
+  {
+    name: "birthCity",
+    type: "autoComplete",
+    label: "Cidade de nascimento",
+    inputWidth: "w-60",
+    loadOptionsFunction: FetchCity,
+  },
+];
